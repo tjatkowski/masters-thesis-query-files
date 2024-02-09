3ffbf22a708c7b1d84d3eb23bdb3df4b0fcb93b2
Tomasz Jatkowski <tomaszjatkowski42@gmail.com>
2024-02-09 15:52:51 +0100

    Create simple setup

--- src/main.py
+++ src/main.py
@@ -1,23 +1,98 @@
-from llama_index.llms import Ollama
-from llama_index import ServiceContext
-from llama_index import VectorStoreIndex, SimpleDirectoryReader
+# # import logging
+# # import sys
+# #
+# # import llama_index
+# # from llama_index.llms import Ollama
+# # from llama_index import ServiceContext
+# from llama_index import VectorStoreIndex, SimpleDirectoryReader
+# #
+# # print("Loading LLama2")
+# # llm = Ollama(model="llama2")
+# #
+# # print("Loading ServiceContext")
+# # service_context = ServiceContext.from_defaults(embed_model="local",
+# #                                                llm=llm)
+# #
+# # print("Loading documents")
+# documents = SimpleDirectoryReader("data").load_data()
+# # print("Loading index")
+# # index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+# #
+# # print("Loading query engine")
+# # query_engine = index.as_query_engine(service_context=service_context, streaming=True)
+# #
+# # print("Querying")
+# # response = query_engine.query("Who is Captain Shannon?")
+# # response.print_response_stream()
+# #
+# # print(response)
+#
+# # Captain Shannon is a mysterious figure who is believed to be the mastermind behind a series of infamous crimes in
+# # England and Ireland. The police have failed to identify him, despite their efforts to uncover his true identity.
+# # He is known to be a ruthless and cunning criminal who will stop at nothing to achieve his goals. His motives are
+# # unknown, but he seems to take great pleasure in terrorizing and intimidating innocent civilians.
+# # The government has even denounced him as a monster and a murderer, vowing to bring him to justice.
+# # However, little is known about Captain Shannon's personal life or background, making him an enigmatic figure.
+#
+#
+# from llama_index import StorageContext
+#
+# from llama_index.llms import Ollama
+# from llama_index import ServiceContext, load_index_from_storage, load_indices_from_storage
+# from llama_index import VectorStoreIndex, SimpleDirectoryReader
+# from llama_index.storage.docstore import SimpleDocumentStore
+# from llama_index.storage.index_store import SimpleIndexStore
+# from llama_index.vector_stores import SimpleVectorStore
+#
+# storage_context = StorageContext.from_defaults()
+#
+#
+# print("Loading LLama2")
+# llm = Ollama(model="llama2")
+#
+# print("Loading ServiceContext")
+# service_context = ServiceContext.from_defaults(embed_model="local",
+#                                                llm=llm)
+#
+# index = load_indices_from_storage(storage_context=storage_context,
+#                                   service_context=service_context)
+#
+# print(index)
+# #
+# # print("Loading documents")
+# # documents = SimpleDirectoryReader("data").load_data()
+# # print("Loading index")
+# # index = VectorStoreIndex.from_documents(documents,
+# #                                         service_context=service_context,
+# #                                         storage_context=storage_context)
+# # storage_context.persist('./local_storage/')
+# #
+# # print("Loading query engine")
+# # query_engine = index.as_query_engine(service_context=service_context,
+# #                                      streaming=True)
+# #
+# # print("Querying")
+# # response = query_engine.query("Who is Captain Shannon?")
+# # response.print_response_stream()
+# #
+# # print(response)
+#
 
-print("Loading LLama2")
-llm = Ollama(model="llama2")
+from service import create_index, add_file_to_index, query_index
 
-print("Loading ServiceContext")
-service_context = ServiceContext.from_defaults(embed_model="local",
-                                               llm=llm)
+create_index('dupa3')
 
-print("Loading documents")
-documents = SimpleDirectoryReader("data").load_data()
-print("Loading index")
-index = VectorStoreIndex.from_documents(documents, service_context=service_context)
 
-print("Loading query engine")
-query_engine = index.as_query_engine(service_context=service_context)
+# query_index('d276f92d-1797-4eb5-b1e1-080eef13005c', 'Who is Captain Shannon?')
 
-print("Querying")
-response = query_engine.query("When exactly party happened at the author's house?")
 
-print(response)
+
+
+
+
+
+
+
+
+
+
---
+++ src/service/__init__.py
@@ -0,0 +1,47 @@
+from llama_index import ServiceContext, Document, VectorStoreIndex, StorageContext, load_index_from_storage
+from llama_index.llms import Ollama
+
+storage_dir = './local_storage'
+
+llm = Ollama(model="llama2")
+
+service_context = ServiceContext.from_defaults(embed_model="local",
+                                               llm=llm)
+
+try:
+    storage_context = StorageContext.from_defaults(persist_dir=storage_dir)
+except FileNotFoundError:
+    storage_context = StorageContext.from_defaults()
+
+
+def create_index(index_id):
+    index = VectorStoreIndex([], service_context=service_context, storage_context=storage_context)
+    if index_id:
+        index.set_index_id(index_id)
+    index.storage_context.persist(persist_dir=storage_dir)
+
+
+def add_file_to_index(index_id, path):
+    index = load_index_from_storage(storage_context=storage_context, index_id=index_id)
+
+    with open(path, 'r') as file:
+        index.insert(Document(text=file.read()))
+        index.storage_context.persist(persist_dir=storage_dir)
+
+
+def remove_file_from_index():
+    pass
+
+
+def query_index(index_id, question):
+    index = load_index_from_storage(storage_context=storage_context,
+                                    service_context=service_context,
+                                    index_id=index_id)
+    print("Loading query engine")
+    query_engine = index.as_query_engine(service_context=service_context,
+                                         streaming=True)
+
+    print("Querying")
+    response = query_engine.query(question)
+    response = query_engine.query("Who is Captain Shannon?")
+    response.print_response_stream()
